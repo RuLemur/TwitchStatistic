1e8355804692ae1a533223735bec88cfd01d45d9
Aleksander Pavlov <rulemur@gmail.com>
2017-10-11 00:57:39 +0300

    добавил TwitchWrapper

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,9 @@
+import com.mb3364.twitch.api.Twitch;
+import com.mb3364.twitch.api.handlers.StreamsResponseHandler;
+import com.mb3364.twitch.api.models.Stream;
+
 import java.sql.*;
+import java.util.List;
 
 /**
  * Created by RuLemur on 19.07.2017.
@@ -12,28 +17,47 @@ public class Main {
 
 
     public static void main(String[] args) {
-        try {
-            Driver driver = new com.mysql.cj.jdbc.Driver();
-            DriverManager.registerDriver(driver);
+//        try {
+//            Driver driver = new com.mysql.cj.jdbc.Driver();
+//            DriverManager.registerDriver(driver);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD); Statement statement = connection.createStatement()) {
+////            statement.execute("INSERT INTO users(name,age,email) VALUES('boris',11,'qwer@asf.as');");
+////            int upd = statement.executeUpdate("UPDATE users SET name = 'Max' where name = 'igor'");
+////            ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
+//            String query = "SELECT * FROM users";
+//            ResultSet resultSet = statement.executeQuery(query);
+//
+//            while (resultSet.next()){
+//                int id = resultSet.getInt(1);
+//                System.out.println(id);
+//            }
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD); Statement statement = connection.createStatement()) {
-//            statement.execute("INSERT INTO users(name,age,email) VALUES('boris',11,'qwer@asf.as');");
-//            int upd = statement.executeUpdate("UPDATE users SET name = 'Max' where name = 'igor'");
-//            ResultSet resultSet = statement.executeQuery("SELECT * FROM users;");
-            String query = "SELECT * FROM users";
-            ResultSet resultSet = statement.executeQuery(query);
-
-            while (resultSet.next()){
-                int id = resultSet.getInt(1);
-                System.out.println(id);
+        Twitch twitch = new Twitch();
+        twitch.setClientId("smo0k1o7t0otmepejy137cg7wrmqz3g");
+        twitch.streams().get(new StreamsResponseHandler() {
+            @Override
+            public void onSuccess(int total, List<Stream> streams) {
+                System.out.println(total + " " + streams.get(1));
             }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
+
+            @Override
+            public void onFailure(int statusCode, String statusMessage, String errorMessage) {
+
+            }
+
+            @Override
+            public void onFailure(Throwable throwable) {
+
+            }
+        });
 
     }
 }
--- target/test-classes/test.sql
+++ target/test-classes/test.sql
@@ -5,3 +5,12 @@ select * from users;
 delete from users where id = 3;
 
 update users set name='igor', age = 15 where id =4;
+
+CREATE TABLE `channels` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) NOT NULL,
+  `FOLLOW_COUNT` int(11) NOT NULL,
+  `LANGUAGE` varchar(45) DEFAULT NULL,
+  `CREATE_DATE` date NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
